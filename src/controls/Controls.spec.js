659ac29f1fe5d26273e21216a6c986e6a31eee48
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-09-16 13:32:36 -0700

    fixed errors, made tests for dashboard

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,36 +1,24 @@
 // Test away
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-import Controls from '../controls/Controls';
+import Controls from "../controls/Controls";
 
+it("the closed toggle button is disabled if the gate is locked", () => {
+  const closedClass = jest.fn();
+  const locked = true;
+  const closed = true;
 
-it('the closed toggle button is disabled if the gate is locked'
-, () => {
+  const { getByText, queryByText } = render(
+    <Controls toggleLocked={toggleLocked} locked={locked} closed={closed} />
+  );
 
-    
-    const closedClass = jest.fn();
-    const locked = true;
-    const closed = true;
-    
-
-    const {getByText, queryByText} = render(<Controls
-        toggleLocked={toggleLocked}
-         locked={locked}
-        closed={closed}
-    />)
-
-
-it('the locked toggle button is disabled if the gate is open'
-, () => {
-
-    
+  it("the locked toggle button is disabled if the gate is open", () => {
     const toggleLocked = jest.fn();
     const locked = true;
     const closed = true;
-    
 
-    const {getByText, queryByText} = render(<Controls
-        toggleLocked={toggleLocked}
-         locked={locked}
-        closed={closed}
-    />);
+    const { getByText, queryByText } = render(
+      <Controls toggleLocked={toggleLocked} locked={locked} closed={closed} />
+    );
+  });
+});
